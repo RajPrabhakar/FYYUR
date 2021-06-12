45d15f05157be1f391b63587e5f980b4cce54034
RajPrabhakar <RajPrabhakar1804@gmail.com>
2021-06-12 18:03:17 +0530

    For GIT

--- config.py
+++ config.py
@@ -4,12 +4,15 @@ SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = False
-ENV = 'prod'
+# DEBUG = False
+# ENV = 'prod'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+DEBUG = True
+ENV = 'development'
 
 # Connect to the database
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://wjszegaakgtnak:db90efe1108441db48d177ab98df9ae679a6fb88e1ac6182e54aa1d4b36fc249@ec2-3-233-7-12.compute-1.amazonaws.com:5432/d721tgln18l8u1'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:alpha@localhost:5432/fyyur'
